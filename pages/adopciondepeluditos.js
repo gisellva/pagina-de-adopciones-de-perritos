60e210f9a427809f32d5fe997c3835a125a3dec5
heidy vanegas <depgisell@gmail.com>
2023-01-17 12:02:21 -0500

    octavo

---
+++ pages/adopciondepeluditos.js
@@ -0,0 +1,27 @@
+
+
+export async function getServerSideProps() {
+const url="http://localhost:3000/api/hello"
+ const res=await fetch(url)
+ const data =await res.json()
+ console.log(data)
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+
+export default function MiPagina({ data }) {
+    
+  return(
+    <div>
+   
+     <div>
+        {data.map(dat => <p key={dat.id}>{dat.nombre} </p>)}
+     </div>
+    </div>
+    
+  );
+}
